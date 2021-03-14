fdd2e7640cb6be68baeef45ce3ef93f1d3bd6d9e
lynchlynch <yepei1988@163.com>
2021-03-14 20:27:34 +0800

    shqc

--- weekly_anly.py
+++ weekly_anly.py
@@ -7,14 +7,19 @@ import matplotlib.pyplot as plt
 import trans_data as td
 import draw_figure as drf
 
-raw_data_dir = '/Users/pei/pydir/week_anly/weekly_data/'
-# raw_data_dir = 'D:/pydir/week_anly/weekly_data/'
+start_date = '2021-03-06'
+end_date = '2021-03-12'
+
+# print(start_date.split('-'))
+raw_data_dir = '/Users/pei/pydir/week_anly/weekly_data/' + str(int(start_date.split('-')[1])) + '.' + \
+               str(int(start_date.split('-')[2])) + '-' +  str(int(end_date.split('-')[1])) + '.' + \
+               str(int(end_date.split('-')[2]))
+# raw_data_dir = 'D:/pydir/week_anly/weekly_data/' + str(int(start_date.split('-')[1])) + '.' + \
+#                str(int(start_date.split('-')[2])) + '-' +  str(int(end_date.split('-')[1])) + '.' + \
+#                str(int(end_date.split('-')[2]))
 
 result_dir = '/Users/pei/pydir/week_anly/result/'
 # result_dir = 'D:/pydir/week_anly/result/'
-
-start_date = '2021-03-06'
-end_date = '2021-03-12'
 
 #delete non-csv file
 for single_file in os.listdir(raw_data_dir):
